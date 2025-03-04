228a0a245430ab4a27cfe5e45986cbec0604574e
mo_aidara <mansouraidara43@gmail.com>
2025-03-04 19:11:55 +0100

    creation du depot git de Projet_PHP

---
+++ login.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+ <!-- section head -->
+ <?php  require_once ("View/sections/login/head.php")?>
+
+<body>
+ <!-- section menu -->
+  <main>
+  <?php  require_once ("View/sections/login/menu.php")?>
+  </main><!-- End #main -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- section script -->
+  <?php  require_once ("View/sections/login/script.php")?>
+
+</body>
+
+</html>
